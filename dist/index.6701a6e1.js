369799137aa6c9c9197c7a869287436fd2dc5ec8
Claire De Lorie <delorie.claire@gmail.com>
2021-08-23 22:22:02 -0400

    Update index.6701a6e1.js

--- dist/index.6701a6e1.js
+++ dist/index.6701a6e1.js
@@ -24266,9 +24266,6 @@ class MovieCard extends _reactDefault.default.Component {
     render() {
         const { movie , onMovieClick  } = this.props;
         return(/*#__PURE__*/ _jsxRuntime.jsxs(_cardDefault.default, {
-            style: {
-                width: "18rem"
-            },
             __source: {
                 fileName: "src/components/movie-card/movie-card.jsx",
                 lineNumber: 11
