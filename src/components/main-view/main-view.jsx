248e271b88cd276bbf1c08361699ed9b2bdc36f3
Claire De Lorie <delorie.claire@gmail.com>
2021-08-23 23:36:51 -0400

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -26,9 +26,48 @@ export class MainView extends React.Component {
       });
   }
 
+<<<<<<< Updated upstream
   setSelectedMovie(newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie,
+=======
+  setSelectedMovie(movie) {
+    this.setState({
+      selectedMovie: movie,
+    });
+  }
+
+  getMovies(token) {
+    axios
+      .get("https://boiling-savannah-13307.herokuapp.com/movies", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  onLoggedIn(authData) {
+    console.log(authData);
+    this.setState({
+      user: authData.user.Username,
+    });
+
+    localStorage.setItem("token", authData.token);
+    localStorage.setItem("user", authData.user.Username);
+    this.getMovies(authData.token);
+  }
+
+  onRegistration(register) {
+    this.setState({
+      register,
+>>>>>>> Stashed changes
     });
   }
 
