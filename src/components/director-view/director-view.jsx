b70aca6b2b744f505a2dda1bb4ff9104bccd8a54
Claire De Lorie <delorie.claire@gmail.com>
2021-08-30 16:08:05 -0400

    Update director-view.jsx
    imported director-view.scss

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -2,23 +2,26 @@ import React from "react";
 import propTypes from "prop-types";
 import Button from "react-bootstrap/Button";
 
+import "./director-view.scss";
+
 export class DirectorView extends React.Component {
   render() {
-    const { movie, onBackClick } = this.props;
+    const { movies, onBackClick } = this.props;
 
     return (
       <div className="director-view">
         <div className="director-name">
           <h1>
-            <span className="value">{movie.Director.Name}</span>
+            <span className="value">{movies.Director.Name}</span>
           </h1>
         </div>
+
         <div className="director-bio">
-          <span className="value">{movie.Director.Bio}</span>
+          <span className="value">{movies.Director.Bio}</span>
         </div>
 
         <div className="director-birthdate">
-          <span className="value">{movie.Director.Birthdate}</span>
+          <span className="value">{movies.Director.Birthdate}</span>
         </div>
 
         <Button
