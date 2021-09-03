9dbd7a6bcc68240b9ce3e9403fbce4543a7df31f
Claire De Lorie <delorie.claire@gmail.com>
2021-09-03 19:21:18 -0400

    Update actions.js
    edited value

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,14 +2,23 @@ export const SET_MOVIES = "SET_MOVIES";
 export const SET_FILTER = "SET_FILTER";
 export const SET_USER = "SET_USER";
 
-export function setMovies(movies) {
-  return { type: SET_MOVIES, value: movies };
+export function setMovies(value) {
+  return {
+    type: SET_MOVIES,
+    value,
+  };
 }
 
-export function setFilter(visibilityFilter) {
-  return { type: SET_FILTER, value: visibilityFilter };
+export function setFilter(value) {
+  return {
+    type: SET_FILTER,
+    value,
+  };
 }
 
-export function setUser(user) {
-  return { type: SET_MOVIES, value: user };
+export function setUser(value) {
+  return {
+    type: SET_MOVIES,
+    value,
+  };
 }
