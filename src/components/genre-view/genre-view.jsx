1c13fbff6a56a344688b024c21259535c3a1cede
Claire De Lorie <delorie.claire@gmail.com>
2021-09-03 22:10:13 -0400

    Update genre-view.jsx
    changed movies.Genre... to genre...

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,35 +1,31 @@
 import React from "react";
 import propTypes from "prop-types";
-import Button from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
 import "./genre-view.scss";
 
-export default class GenreView extends React.Component {
-  render() {
-    const { movies, onBackClick } = this.props;
-
-    return (
-      <div className="genre-view">
-        <div className="genre-name">
-          <h1>
-            <span className="value">{movies.Genre.Name}</span>
-          </h1>
-        </div>
-        <div className="genre-description">
-          <span className="value">{movies.Genre.Description}</span>
-        </div>
-
-        <Button
-          variant="primary"
-          onClick={() => {
-            onBackClick(null);
-          }}
-        >
-          Back
-        </Button>
+export function GenreView({ genre, onBackClick }) {
+  return (
+    <div className="genre-view">
+      <div className="genre-name">
+        <h1>
+          <span className="value">{genre.Name}</span>
+        </h1>
       </div>
-    );
-  }
+      <div className="genre-description">
+        <span className="value">{genre.Description}</span>
+      </div>
+
+      <Button
+        variant="primary"
+        onClick={() => {
+          onBackClick(null);
+        }}
+      >
+        Back
+      </Button>
+    </div>
+  );
 }
 
 GenreView.propTypes = {
